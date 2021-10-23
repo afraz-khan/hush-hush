1c4e30e57786c641cfdfca0b5d6e38837a07598c
Afraz Khan <ah.afrazkhan@gmail.com>
2021-10-24 03:13:02 +0500

    heroku deploy test

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-app = Flask(__name__, static_folder='client/build', static_url_path='')
+app = Flask(__name__, static_folder='client/build', static_url_path='/')
 # CORS(app, origins=['https://hush-hush-demo.herokuapp.com', 'https://code.jquery.com', 'https://stackpath.bootstrapcdn.com',
 # 					'https://cdnjs.cloudflare.com'], methods=['POST', 'PUT', 'GET', 'DELETE'],
 # 					allow_headers=['Authorization'])
